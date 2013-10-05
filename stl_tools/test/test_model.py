42b16cf3a7fa3882ee344fb081158241ed994787
Tristan A. Hearn <tristan.a.hearn@nasa.gov>
2013-10-05 17:00:33 -0400

    unittests added

---
+++ stl_tools/test/test_model.py
@@ -0,0 +1,54 @@
+import logging
+import os
+import unittest
+
+import numpy as np
+from scipy.misc import imread
+from stl_tools import text2array, numpy2stl
+
+
+"""
+Some basic tests for stl_tools
+"""
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestSTL(unittest.TestCase):
+
+    def test_text(self):
+        """ Tests creation of an image array from a text expression.
+        Covers the text2png and text2array functions.
+        """
+
+        A = text2array("TEST", fontsize=50)[:, :, 0]
+        B = np.loadtxt("TEST.csv", delimiter=',')
+        r = np.linalg.norm(A - B) / np.linalg.norm(B)
+        self.assertAlmostEqual(r, 0.0)
+
+    def test_png(self):
+        """ Tests creation of an STL from a PNG.
+        Covers the text2array function.
+        """
+        # test ascii output
+        A = imread("TEST.png")
+        numpy2stl(A, "OUT_.stl", scale=0.05, mask_val=6., ascii=True)
+        reference = open("TEST_ascii.stl", 'rb')
+        new = open("OUT_.stl", 'rb')
+        newlines = new.readlines()
+        reflines = reference.readlines()
+        new.close(), reference.close()
+        self.assertEqual(newlines, reflines)
+
+        # test binary output
+        numpy2stl(A, "OUT_.stl", scale=0.05, mask_val=6.)
+        reference = open("TEST_bin.stl", 'rb')
+        new = open("OUT_.stl", 'rb')
+        newlines = new.readlines()
+        reflines = reference.readlines()
+        new.close(), reference.close()
+        self.assertEqual(newlines, reflines)
+        os.remove("OUT_.stl")
+
+if __name__ == '__main__':
+    unittest.main()
