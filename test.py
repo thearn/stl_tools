3564857a630e6d93fd3feef20becba50663fbe67
Ramzi Abdoch <ramzi_abdoch@mckinsey.com>
2018-05-14 23:48:50 -0400

    running successfully on prepared images 😁

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
+import os
+import skimage
+
 from stl_tools import numpy2stl
 from pylab import imread
+from skimage import io
 from skimage.transform import resize
 
-A = 256 * imread("examples/01.png")
-image = resize(A, (256, 256))
+A = io.imread("examples/01.png")
+A = A[:, :, 2] + 1.0*A[:,:, 0]
 
-numpy2stl(A, "output/madeleine.stl", scale=0.1, solid=False)
+numpy2stl(A, "madeleine.stl", scale=0.05, mask_val=5., solid=True)
