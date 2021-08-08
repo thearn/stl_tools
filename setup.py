40b246a4dafe12cf64b0f013ade7ff9920f553dc
MarkKoz <KozlovMark@gmail.com>
2021-08-08 14:39:50 -0700

    Specify minimum versions for dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import dist, setup, Extension
 
-dist.Distribution().fetch_build_eggs(['numpy'])
+dist.Distribution().fetch_build_eggs(['numpy>=1.7'])
 import numpy as np
 
 SRC_DIR = "stl_tools"
@@ -14,7 +14,8 @@ EXTENSIONS = [ext_1]
 
 setup(name='stl_tools',
       version='0.3.0',
-      install_requires=['numpy', 'scipy', 'matplotlib'],
+      install_requires=['numpy>=1.7', 'scipy>=0.12', 'matplotlib>=1.2.1'],
+      python_requires='>=2.7,!=3.0.*,!=3.1.*',
       description="Generate STL files from numpy arrays and text",
       author='Tristan Hearn',
       author_email='tristanhearn@gmail.com',
@@ -22,7 +23,7 @@ setup(name='stl_tools',
       license='Apache 2.0',
       packages=['stl_tools'],
       ext_modules=EXTENSIONS,
-      setup_requires=['cython'],
+      setup_requires=['cython>=0.17.4'],
       entry_points={
           'console_scripts':
           ['image2stl=stl_tools.image2stl:image2stl']
